93e96bc40664505c33bfb8517215ae11476936c3
TBusterT <tarastsupak120@gmail.com>
2024-09-09 01:45:30 +0300

    init commit

---
+++ Exams/index.js
@@ -0,0 +1,35 @@
+const usersSection = document.getElementById('usersSection');
+document.body.appendChild(usersSection)
+
+const container = document.getElementById('usersContainer');
+usersSection.appendChild(container);
+
+let usersList = document.getElementById('users-list');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+      for (const user of users) {
+        let userItem = document.createElement('li');
+        userItem.classList.add('user-item');
+
+        let userId = document.createElement('h2');
+        userId.classList.add('user-id');
+        userId.innerText = `ID: ${user.id}`;
+
+        let userName = document.createElement('h3');
+        userName.classList.add('card-info', 'user-name');
+        userName.innerText = `${user.name}`;
+
+        let userDetailsBtn = document.createElement('a');
+        userDetailsBtn.classList.add('btn');
+        userDetailsBtn.href = `user-details.html?id=${user.id}`;
+        userDetailsBtn.innerText = 'User details';
+
+        userItem.append(userId, userName, userDetailsBtn);
+        usersList.appendChild(userItem);
+      }
+    });
+});
---
+++ Exams/post-details.js
@@ -0,0 +1,75 @@
+const postSection = document.getElementById('postSection');
+document.body.appendChild(postSection);
+
+const container = document.getElementById('postContainer');
+postSection.appendChild(container);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const idPost = urlParams.get('id');
+
+  fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`)
+    .then(response => response.json())
+    .then(post => {
+      const container = document.getElementById('postContainer');
+      const postBlock = document.createElement('div');
+      postBlock.classList.add('post-details');
+
+      const postBlockTitle = document.createElement('h1');
+      postBlockTitle.classList.add('main-title', 'post-main-title');
+      postBlockTitle.innerText = `${post.title}`;
+
+      const postBlockText = document.createElement('p');
+      postBlockText.classList.add('post-block-text');
+      postBlockText.innerText = `${post.body}`;
+
+      const commentsTitle = document.createElement('h2');
+      commentsTitle.classList.add('main-title');
+      commentsTitle.innerText = 'Comments';
+
+      postBlock.append(postBlockTitle, postBlockText)
+      container.append(postBlock, commentsTitle);
+
+
+      fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
+        .then(response => response.json())
+        .then(comments => {
+          const commentsList = document.createElement('ul');
+          commentsList.classList.add('comments-list');
+
+          comments.forEach(comment => {
+            const commentItem = document.createElement('li');
+            commentItem.classList.add('comment-item');
+
+            const commentInfoList = document.createElement('ul');
+            commentInfoList.classList.add('comment-info-list');
+
+            commentsList.appendChild(commentItem);
+            commentItem.appendChild(commentInfoList);
+
+            for (const commentKey in comment) {
+              const commentInfoItem = document.createElement('li');
+              commentInfoItem.classList.add('comment-info-item');
+
+              const commentInfoTitle = document.createElement('h2');
+              commentInfoTitle.classList.add('comment-info-title');
+              commentInfoTitle.innerText = `${commentKey}`;
+
+              const commentInfoText = document.createElement('p');
+              commentInfoText.classList.add('comment-info-text');
+              commentInfoText.innerText = `${comment[commentKey]}`;
+
+              commentInfoItem.append(commentInfoTitle, commentInfoText);
+              commentInfoList.appendChild(commentInfoItem);
+            }
+          });
+
+          container.appendChild(commentsList);
+
+          const backPageWrapper = document.createElement('div');
+          backPageWrapper.classList.add('back-page-wrapper');
+          backPageWrapper.innerHTML = `<a class="btn" onclick="history.back(); return false;">Back</a>`;
+          container.appendChild(backPageWrapper);
+        });
+    });
+});
